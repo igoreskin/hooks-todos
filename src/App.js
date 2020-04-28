0634223f0e3e02bac749cf9990b1479377c05abb
Igor Eskin <eskinhd@gmail.com>
2020-04-28 15:42:29 -0400

    useReducer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
-import React, { useContext } from 'react';
+import React, { useContext, useReducer } from 'react';
 import { UserContext } from './index';
 
-export default function App() {
+const initialState = {
+  count: 0
+}
 
+function reducer(state, action) {
+  switch(action.type) {
+    case "increment":
+      return { count: state.count + 1 }
+    case "decrement":
+      return { count: state.count - 1 }
+    default:
+      return initialState;
+  }
+}
+
+export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState)
   const value = useContext(UserContext);
 
   return (
     <div>
-        <div>Hello, {value}</div>
+      <div>Count: {state.count}</div>
+      <button className="border p-1" onClick={() => dispatch({ type: "increment" })}>Increment</button>
+      <button className="border p-1" onClick={() => dispatch({ type: "decrement" })}>Decrement</button>
     </div>
   );
 }
